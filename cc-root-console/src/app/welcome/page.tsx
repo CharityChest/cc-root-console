02c6f7090d318fc97518bc851b1760872ce1c4c4
Domenico Rizzo <domenico.rizzo@gmail.com>
2023-08-03 16:50:34 +0200

    Add welcome page component
    This commit adds a new file `page.tsx` which contains the welcome page component. The component imports `React` and `Layout` from their respective modules, and renders a layout with a heading "Welcome".

---
+++ cc-root-console/src/app/welcome/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Layout from '@/component/layout';
+
+const Welcome : React.FC = () => {
+    return (
+        <Layout>
+            <div>
+                <h1>Welcome</h1>
+            </div>
+        </Layout>
+    );
+}
+
+export default Welcome;
